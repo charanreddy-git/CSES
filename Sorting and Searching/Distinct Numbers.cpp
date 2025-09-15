6491c5ac9ef4d61bd6c9753100c801e1962bec50
Charan Reddy <charanreddy.puchala@gmail.com>
2025-09-15 10:07:36 +0530

    Add Distinct Numbers program in C++

---
+++ Sorting and Searching/Distinct Numbers.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    int cnt = 1;
+    for (int i = 0; i < n-1; i++) {
+        if (a[i] != a[i+1]) cnt++;
+    }
+    cout << cnt << '\n';
+    return 0;
+}
